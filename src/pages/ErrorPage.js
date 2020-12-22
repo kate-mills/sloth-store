e60a806097085a59bb14a21e16b7e70b88541a39
Kate Mills <kate.mills1124@gmail.com>
2020-12-22 12:41:42 -0800

    Error page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -2,7 +2,15 @@ import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 const ErrorPage = () => {
-  return <h4>error page</h4>
+  return (
+    <Wrapper className="page-100">
+      <section>
+        <h1>Oops!</h1>
+        <h3>Sorry, the page you tried cannot be found.</h3>
+        <Link to="/" className="btn">Back to Homepage</Link>
+      </section>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.main`
