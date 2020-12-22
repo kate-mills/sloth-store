e605e0513e0e916d1cec83d82c68bae6e95d1bf6
Kate Mills <kate.mills1124@gmail.com>
2020-12-22 10:29:57 -0800

    Raleway font

--- src/index.js
+++ src/index.js
@@ -7,5 +7,6 @@ import { FilterProvider } from './context/filter_context'
 import { CartProvider } from './context/cart_context'
 import { UserProvider } from './context/user_context'
 import { Auth0Provider } from '@auth0/auth0-react'
+import "fontsource-raleway";
 
 ReactDOM.render(<App />, document.getElementById('root'))
